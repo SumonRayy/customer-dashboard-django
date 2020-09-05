a6e7ae3386b6cd18574b296acb6648b619e066a3
SumonRayy <sumonrayy@gmail.com>
2020-09-05 07:40:50 +0530

    new url paths added: update_order & delete_order

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home),
-    path("products/", views.products),
-    path("about/", views.contact),
-    path("customer/", views.customer),
+    path("", views.home, name="home"),
+    path("products/", views.products, name="products"),
+    path("about/", views.contact, name="about"),
+    path("customer/<str:pk>/", views.customer, name="customer"),
+    path("create_order/", views.createOrder, name="create_order"),
+    path("update_order/<str:pk_up>", views.updateOrder, name="update_order"),
+    path("delete_order/<str:pk_del>", views.deleteOrder, name="delete_order"),
 ]
